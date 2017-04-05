2f3a68fe5485b366a831eb1b784495db9c2f0a65
katja.leibold <katja.leibold@if1-pc29.schulnetz.aeg-online.de>
2017-04-05 16:23:31 +0200

    Extract_Min

--- src/MinimumHeapScheduler.java
+++ src/MinimumHeapScheduler.java
@@ -22,14 +22,8 @@ public class MinimumHeapScheduler implements Scheduler {
 		return null;
 	}
 
-	@Override
-	public void schedule(Event e) {
-		n++;
-		if (root == null) {
-			root = new MinimumHeapNode(e, null, null, null);
-			return;
-		}
-
+	
+	private MinimumHeapNode getNodeAtIndex(int n, boolean enter){
 		int z = 1;
 		while (z <= n) {
 			z = z * 2;
@@ -50,9 +44,35 @@ public class MinimumHeapScheduler implements Scheduler {
 				aktuell = aktuell.getlSon(); // zu dem linken Son (n+0)
 			}
 		}
+		
+		if(enter){
+			if (m == 0) {
+				//links
+				aktuell = aktuell.getlSon(); // zu dem linken Son (n+0)
+			} else {
+				aktuell = aktuell.getrSon(); // zu dem rechten Son (n+1)
+				//rechts		
+			}
+		}
+		
+		return aktuell;
+	}
+	
+	
+	@Override
+	public void schedule(Event e) {
+		n++;
+		if (root == null) {
+			root = new MinimumHeapNode(e, null, null, null);
+			return;
+		}
+
+		
+		MinimumHeapNode aktuell = getNodeAtIndex(n, false);
+		
 
 		MinimumHeapNode newNode = new MinimumHeapNode(e, aktuell, null, null);
-     	if (m == 0) {
+     	if (aktuell.getlSon() == null) {
 			//links
      		aktuell.setlSon(newNode);
 		} else {
@@ -66,6 +86,51 @@ public class MinimumHeapScheduler implements Scheduler {
      		newNode.setEvent(temp);
      		newNode = newNode.getFather();
      	}     	
+	}
+	
+	
+	
+	
+	public Event extractMin(){
+		// man entfernt das Element aus der Wurzel 
+		// -> fügt das letzte Element des Baums in die Wurzel
+		// -> wenn größer als Sohn, dann tauschen, sodass die kleinste Zahl oben steht
+		if(root==null){
+			return null;
+		}
+		Event ev = root.getEvent();
+		if(n==1){
+			root=null;
+			n=0;
+			return ev;
+		}
+		
+		MinimumHeapNode aktuell = getNodeAtIndex(n, true);
+		root.setEvent(aktuell.getEvent());
+		
+		if(aktuell.getFather().getlSon()== aktuell){
+		    aktuell.getFather().setlSon(null);
+		}else{
+			aktuell.getFather().setrSon(null);
+		}
+		
+
+		aktuell = root;
+		while(aktuell.getEvent().getTs() > aktuell.getlSon().getEvent().getTs() || aktuell.getEvent().getTs() > aktuell.getrSon().getEvent().getTs() ){
+			Event temp = aktuell.getEvent();
+			if (aktuell.getlSon().getEvent().getTs() > aktuell.getrSon().getEvent().getTs()) {
+				aktuell.setEvent(aktuell.getrSon().getEvent());
+				aktuell.getrSon().setEvent(temp);
+				aktuell = aktuell.getrSon();
+			} else {
+				aktuell.setEvent(aktuell.getlSon().getEvent());
+				aktuell.getlSon().setEvent(temp);
+				aktuell = aktuell.getlSon();
+			}
+		}
+		// TODO null Pointer exception beachten
+		
+		return ev;
 	}
 	
 	
