8b6fe5964c80b53b0e6957d84e6aa28f45ab253a
felix.stach <felix.stach@if1-pc27.schulnetz.aeg-online.de>
2017-04-05 15:01:14 +0200

    again?

--- src/Node.java
+++ src/Node.java
@@ -7,22 +7,27 @@ import java.util.*;
 
 public class Node {
 
-	private double x;
-	private double y;
-	private NetworkInterface netInf = new NetworkInterface(null, null);													//Standard-NetInf
-	private LinkedList<Protocol> protocols = new LinkedList<Protocol>();
-	
-	public Node(int x, int y ) {
-		this.x = x; 
-		this.y = y;
-		protocols.add(netInf);
-	}
-	
-	public void addProtocol(Protocol p) {
-		Protocol last = protocols.getLast();
-		
-		last.setAbove(p);
-		protocols.add(p);
-		p.setBelow(last);	
-	}
+  private double x;
+  private double y;
+  private NetworkInterface netInf = new NetworkInterface(null, null);                         //Standard-NetInf
+  private LinkedList<Protocol> protocols = new LinkedList<Protocol>();
+  
+  public Node(int x, int y ) {
+    this.x = x; 
+    this.y = y;
+    protocols.add(netInf);
+  }
+  
+  public void addProtocol(Protocol p) {
+    Protocol last = protocols.getLast();
+    
+    last.setAbove(p);
+    protocols.add(p);
+    p.setBelow(last); 
+  }
+  
+  private LinkedList<Protocol> getProtocols() {
+  return protocols;
+    }
+  
 }
--- src/World.java
+++ src/World.java
@@ -1,33 +1,41 @@
 import java.util.Vector;
 
 public class World {
-	private Vector<Node> knotenliste = new Vector<Node>();
-	
-	private static Scheduler sched = null;
-	
-	public static Scheduler getScheduler() {
-		if (sched == null) {
-			sched = new SimpleScheduler();
-		}
-		return sched;
-	}
-	
-	
-	public static void main (String args []) {
-		
-		Node node1 = new Node(10,10);
-		Node node2 = new Node(20,20);
-		
-		SimpleProtocol protocol1 = new SimpleProtocol(null, null);
-		SimpleProtocol protocol2 = new SimpleProtocol(null, null);
-		App standardApp = new App(null, null);
-		
-		node1.addProtocol(protocol1);
-		node1.addProtocol(protocol2);
-		node1.addProtocol(standardApp);
-		
-		node2.addProtocol(protocol1);
-		node2.addProtocol(protocol2);
-		node2.addProtocol(standardApp);
-	}
+  private Vector<Node> knotenliste = new Vector<Node>();
+  
+  private static Scheduler sched = null;
+  
+  public static Scheduler getScheduler() {
+    if (sched == null) {
+      sched = new SimpleScheduler();
+    }
+    return sched;
+  }
+  
+  
+  public static void main (String args []) {
+    
+    Node node1 = new Node(10,10);
+    Node node2 = new Node(20,20);
+    
+    SimpleProtocol protocol1 = new SimpleProtocol(null, null);
+    SimpleProtocol protocol2 = new SimpleProtocol(null, null);
+    App standardApp = new App(null, null);
+    
+    
+    SimpleProtocol protocol3 = new SimpleProtocol(null, null);
+    SimpleProtocol protocol4 = new SimpleProtocol(null, null);
+    App tolleApp = new App(null, null);
+    
+    node1.addProtocol(protocol1);
+    node1.addProtocol(protocol2);
+    node1.addProtocol(standardApp);
+    
+    node2.addProtocol(protocol3);
+    node2.addProtocol(protocol4);
+    node2.addProtocol(tolleApp);
+    
+    node2.getProtocols().getFirst().processEvent();
+    
+  }
 }
