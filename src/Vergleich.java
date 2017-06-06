fb2f656cdf1442b8e68afbb8084eb0a6ba5f0e81
Katja Leibold <Katja Leibold@DESKTOP-3IVET4T.fritz.box>
2017-06-06 14:56:38 +0200

    Vergleich

--- src/Vergleich.java
+++ src/Vergleich.java
@@ -1,5 +1,13 @@
+import java.util.Random;
 
 public class Vergleich {
+   private Random zfl =new Random();
    
+   public Vergleich(){
+	   MinimumHeapScheduler b = new MinimumHeapScheduler();
+	   b.schedule(e);
+	   //wie generiere ich zufällige Events und timestanps???
+	   
+   }
 	
 }
