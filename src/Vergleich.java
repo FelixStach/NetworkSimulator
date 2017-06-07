95e93e8b4b041e9548e734e8cabe2f8566ba9dc2
katja.leibold <katja.leibold@if1-pc29.schulnetz.aeg-online.de>
2017-06-07 16:41:05 +0200

    Vergleich fertig

--- src/Vergleich.java
+++ src/Vergleich.java
@@ -1,13 +1,39 @@
-import java.util.Random;
+
 
 public class Vergleich {
-   private Random zfl =new Random();
-   
-   public Vergleich(){
-	   MinimumHeapScheduler b = new MinimumHeapScheduler();
-	   b.schedule(e);
-	   //wie generiere ich zufällige Events und timestanps???
-	   
+	public static void main(String[] args) {
+/*		for (int n = 10; n <= 50000; n += 100)
+			runTestSimple(n);*/
+		for (int n = 10; n <= 50000; n += 100)
+			runTestMinHeap(n);
+	}
+	
+	public static void runTestSimple(long n) {
+		 SimpleScheduler s = new SimpleScheduler();
+		 double t = measureTime(s,n);
+		 System.out.printf("Simple: %d = %.6f\n", n, t);
+	}
+
+	public static void runTestMinHeap(long n) {
+		 MinimumHeapScheduler p = new MinimumHeapScheduler();		 
+		 double t = measureTime(p,n);
+		 System.out.printf("MinHeap: %d = %.6f\n", n, t);
+	}
+	
+	
+	public static void scheduleRandomEvents(Scheduler scheduler, long n) {
+		for(long i = 0; i < n; i++) {
+			double ts = (Math.random()*1000);
+			Event e = new Event(ts,null,null,null);
+			scheduler.schedule(e);
+		}
    }
+	
+	public static double measureTime(Scheduler scheduler, long n){
+		long time_start = System.nanoTime();
+		scheduleRandomEvents(scheduler, n);
+		long time_diff = System.nanoTime()- time_start;
+		return time_diff / 1000000000.0;
+	}
 	
 }
